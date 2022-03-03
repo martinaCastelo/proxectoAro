ebac35cb52a04e9547b7174d927d192a078f4c26
Martina Castelo <a21martinacp@iessanclemente.net>
2022-03-04 00:49:05 +0100

    Segundo commit. Refactorización: creación da rama Refactorizacion. Cambia nome do proxecto (pasa a ser proxectoAro). Renomea a clase Circulo por Aro. Renomea o método obterArea por obterSuperficie. Renomea os campos x e y por coordenada X e coordenadaY. Introduce a constante LIMITERADIO. Cambia os parámetros do método trasladarCentro (int trasladarX,int trasladarY) e fai os cambios precisos para engadir dito valor a coordenadaX e coordenadaY. Encapsula coordenadaX, coordenadaY e radio. Elimina con Safety Delete os métodos obterX, obterY, obterRadio, establecerX, establecerY, e establecerRadio.

---
+++ src/main/java/circulo/Aro.java
@@ -0,0 +1,95 @@
+package circulo;
+
+/**
+ * Probas de refactorización en NetBeans coa clase Aro
+ * @author MartinaCastelo
+ */
+public class Aro {
+    
+    public static final double MINIMO = 0.0;
+    private static final double LIMITERADIO = MINIMO;
+    
+    
+    private int coordenadaX;
+    private int coordenadaY;
+    private double radio;
+    
+    /**
+     * @return the coordenadaX
+     */
+    public int getCoordenadaX() {
+        return coordenadaX;
+    }
+
+    /**
+     * @param coordenadaX the coordenadaX to set
+     */
+    public void setCoordenadaX(int coordenadaX) {
+        this.coordenadaX = coordenadaX;
+    }
+
+    /**
+     * @return the coordenadaY
+     */
+    public int getCoordenadaY() {
+        return coordenadaY;
+    }
+
+    /**
+     * @param coordenadaY the coordenadaY to set
+     */
+    public void setCoordenadaY(int coordenadaY) {
+        this.coordenadaY = coordenadaY;
+    }
+
+    /**
+     * @return the radio
+     */
+    public double getRadio() {
+        return radio;
+    }
+
+    /**
+     * @param radio the radio to set
+     */
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public Aro() {
+    }
+
+    public Aro(int valorX, int valorY, double valorRadio) {
+        coordenadaX = valorX;
+        coordenadaY = valorY;
+        setRadio(valorRadio);
+    }
+
+
+
+
+
+
+
+    public double obterDiametro() {
+        return getRadio() * 2;
+    }
+
+    public double obterCircunferencia() {
+        return Math.PI * obterDiametro();
+    }
+
+    public double obterSuperficie() {
+        return Math.PI * getRadio() * getRadio();
+    }
+
+    @Override
+    public String toString() {
+        return "Centro = [" + getCoordenadaX() + "," + getCoordenadaY() + "]; Radio = " + getRadio();
+    }
+
+    public void trasladarCentro(int trasladarX, int trasladarY){
+        setCoordenadaX(getCoordenadaX() + trasladarX);
+        setCoordenadaY(getCoordenadaY() + trasladarY);
+    }
+}
---
+++ src/test/java/circulo/AroTest.java
@@ -0,0 +1,74 @@
+/**
+ * Probas para a clase Circulo
+ * @author MartinaCastelo
+ */
+package circulo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author MartinaCastelo
+ */
+public class AroTest {
+    
+    public AroTest() {
+    }
+
+    /**
+     * Test of obterDiametro method, of class Aro.
+     */
+    @Test
+    public void testObterDiametro() {
+        System.out.println("obterDiametro");
+        Aro instance = new Aro(0,0,0.1);
+        double result = instance.obterDiametro();
+        assertEquals(0.2, result,0);
+    }
+
+    /**
+     * Test of obterCircunferencia method, of class Aro.
+     */
+    @Test
+    public void testObterCircunferencia() {
+        System.out.println("obterCircunferencia");
+        Aro instance = new Aro(0,0,0.1);
+        double expResult = 0.6283185;
+        double result = instance.obterCircunferencia();
+        assertEquals(expResult, result,1e-6);
+    }
+
+    /**
+     * Test of obterSuperficie method, of class Aro.
+     */
+    @Test
+    public void testObterSuperficie() {
+        System.out.println("obterArea");
+        Aro instance = new Aro(0,0,0.1);
+        double expResult = 0.0314159;
+        double result = instance.obterSuperficie();
+        assertEquals(expResult, result,1e-6);
+    }
+
+    /**
+     * Test of trasladarCentro method, of class Aro.
+     */
+    @Test
+    public void testTrasladarCentro() {
+        System.out.println("trasladarCentro");
+        Aro instance = new Aro();
+        int resultx=instance.getCoordenadaX();
+        int resulty=instance.getCoordenadaY();
+        instance.trasladarCentro(5, 6);
+        int resultnx = instance.getCoordenadaX();
+        int resultny = instance.getCoordenadaY();
+        assertEquals(resultx+5, resultnx);
+        assertEquals(resulty+6, resultny);
+    }
+    
+}
